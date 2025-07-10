e1d58acb5f133f6c9315811df49c275f70486119
Tatarinova Polina <polandddddya@gmail.com>
2025-07-10 19:19:57 +0300

    Update

--- app.py
+++ app.py
@@ -75,5 +75,4 @@ def main():
     return "ok", 200
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, threaded=True)
+    app.run(host="0.0.0.0", port=5000, threaded=True)
